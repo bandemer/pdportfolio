bf5f4c41b9acc44975b434539e9b3fed81833f92
Matthias Bandemer <git@bandemer.net>
2019-03-15 23:02:18 +0100

    Upgrade auf Font Awesome 5, Icons aktualisiert

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <title>Pascal Dupré</title>
 <meta name="description" content="Pascal Dupré ist ein Podcaster aus Dresden.">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/bootstrap.css?date=20190315" rel="stylesheet">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 <link rel="icon" href="/favicon.ico">
 </head>
